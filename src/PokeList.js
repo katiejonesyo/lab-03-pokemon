12ccf70f005927f1755ba5724c1c3fad656c555f
Katelinn Jones <katiejonesyo@gmail.com>
2020-10-28 17:05:29 -0700

    poke list done

--- src/PokeList.js
+++ src/PokeList.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { pokemon } from './Data.js';
 import PokeItem from './PokeItem.js';
 
 export default class PokeList extends Component {
-
-
     render () {
+        return (
+            <sections className="poke-list">
+                PokeList (children: PokeItem)
+                {
+                    this.props.data.map((pokemon, index) => {
+                        return (
+                            <PokeItem
+                            name={pokemon.pokemon}
+                            typeOne={pokemon.type_1}
+                            typeTwo={pokemon.type_2}
+                            image={pokemon.url_image}
+                            key={index}/>
+                            )
+                        })
+                }
+            </sections>
+        )
       
     }
-}
+};
